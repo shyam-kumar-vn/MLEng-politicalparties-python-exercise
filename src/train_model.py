862aa728eb7bd771b472370cba54217e5247193b
Shyam Kumar V N <shyamkumar.vn@thoughtworks.com>
2025-06-29 21:41:59 +0530

    update

--- src/train_model.py
+++ src/train_model.py
@@ -32,7 +32,6 @@ def train_model(X_train, y_train, X_test, y_test, model_type="XGBoost", max_iter
             learning_rate=0.1,
             random_state=random_state,
             eval_metric='logloss',
-            use_label_encoder=False,
             verbosity=0
         )
     elif model_type == "LogisticRegression":
